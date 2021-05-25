fd186e92be9ffd898b0a3b8d2579dc47948774ed
shubha507 <69707229+shubha507@users.noreply.github.com>
2021-05-25 12:02:26 +0530

    Revert "Shubha"

--- Grocery App/AppDelegate.swift
+++ Grocery App/AppDelegate.swift
@@ -6,8 +6,6 @@
 //
 
 import UIKit
-import FirebaseCore
-import FirebaseFirestore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,9 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
-        let db = Firestore.firestore()
-        print(db)
         return true
     }
 
